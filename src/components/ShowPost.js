4ae77e823b9b566962b21aecfd6c4eae97d268de
shaunhailey <ediblelysol@gmail.com>
2017-09-01 10:05:17 -0400

    initial commit

---
+++ src/components/ShowPost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const { id } = this.props.match.params
+const URL = `https://tiny-lasagna-server.herokuapp.com/collections/blogger/${id}`
+//
+// The URL variable should be used to fetch the blog post with the _id that matches
+// the endpoint.
+//
+// You can use this.props.history.push('/'); at the end of a fetch in your .then()
+// methods to push to a particular endpoint, such as /.
